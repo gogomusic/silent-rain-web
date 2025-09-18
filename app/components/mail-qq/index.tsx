cb9885ca756973ee8154a5bb1a85a0d21cd876f3
gogomusic <1450820572@qq.com>
2025-09-18 15:09:24 +0800

    chore: 配置prettier、eslint、commitlint等

--- app/components/mail-qq/index.tsx
+++ app/components/mail-qq/index.tsx
@@ -1,18 +1,19 @@
-const email = process.env.EMAIL || "";
+const email = process.env.EMAIL || '';
+import Image from 'next/image';
 
 const MailQq: React.FC = () => {
-  const mailTo = email ? `mailto:${email}` : "";
-  if (!mailTo) {
-    return null;
-  }
-  return (
-    <a href={mailTo} className="no-underline">
-      <img
-        src="http://rescdn.qqmail.com/zh_CN/htmledition/images/function/qm_open/ico_mailme_11.png"
-        alt="通过QQ邮箱联系我"
-      />
-    </a>
-  );
+	const mailTo = email ? `mailto:${email}` : '';
+	if (!mailTo) {
+		return null;
+	}
+	return (
+		<a href={mailTo} className="no-underline">
+			<Image
+				src="http://rescdn.qqmail.com/zh_CN/htmledition/images/function/qm_open/ico_mailme_11.png"
+				alt="通过QQ邮箱联系我"
+			/>
+		</a>
+	);
 };
 
 export default MailQq;
--- app/web/parallax-scrolling/parallax-scrolling-2/page.tsx
+++ app/web/parallax-scrolling/parallax-scrolling-2/page.tsx
@@ -1,16 +1,16 @@
-import styles from "./page.module.scss";
+import styles from './page.module.scss';
 
 const Page: React.FC = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.wallpaper}>
-        <div className={styles.title}>PARALLAX</div>
-      </div>
-      <div className={styles.normal}>
-        <div className={styles.title}>NORMAL</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.wallpaper}>
+				<div className={styles.title}>PARALLAX</div>
+			</div>
+			<div className={styles.normal}>
+				<div className={styles.title}>NORMAL</div>
+			</div>
+		</div>
+	);
 };
 
 export default Page;
